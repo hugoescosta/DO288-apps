55ead0ebeedf1bbccbd5901269e9e29589c8a0d9
Student User <student@workstation.lab.example.com>
2023-08-14 10:07:45 -0400

    Adicionando a varivel IP

--- java-serverhost/src/main/java/com/redhat/training/example/javaserverhost/rest/ServerHostEndPoint.java
+++ java-serverhost/src/main/java/com/redhat/training/example/javaserverhost/rest/ServerHostEndPoint.java
@@ -15,12 +15,11 @@ public class ServerHostEndPoint {
     String host = "";
     try {
       host = InetAddress.getLocalHost().getHostName();
-      ip = InetAddress.getLocalHost().getHostAddress();
     }
     catch (Exception e) {
        e.printStackTrace();
     }
-    String msg = "I am running on server "+host+" Version 1.0 my IP "+ip+"\n";
+    String msg = "I am running on server "+host+" Version 1.0"\n";
     return Response.ok(msg).build();
   }
 }
